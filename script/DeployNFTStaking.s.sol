ee49716d1c33d228bc0d13b9e80fe138e4b70107
Chitranshu <varshneychinu1204@gmail.com>
2024-08-01 18:56:06 +0530

    m

--- script/DeployNFTStaking.s.sol
+++ script/DeployNFTStaking.s.sol
@@ -2,22 +2,30 @@
 pragma solidity 0.8.24;
 
 import "forge-std/Script.sol";
-import "../src/NFTStaking.sol";
+import "../src/nftStaking.sol";
 import "../src/collection.sol";
 import "../src/rewards.sol";
 
 contract DeployNFTStaking is Script {
-    address initialOwner = 0xYourInitialOwnerAddressHere; // Replace with the initial owner address
+    address initialOwner = address(0); // Replace with the initial owner address
 
     function run() external {
         // Start broadcasting transactions
         vm.startBroadcast();
 
         // Deploy Collection contract
-        Collection collection = new Collection("Test Collection", "TEST", initialOwner);
+        Collection collection = new Collection(
+            "Test Collection",
+            "TEST",
+            initialOwner
+        );
 
         // Deploy N2DRewards contract
-        N2DRewards rewards = new N2DRewards("Reward Token", "RTK", initialOwner);
+        N2DRewards rewards = new N2DRewards(
+            "Reward Token",
+            "RTK",
+            initialOwner
+        );
 
         // Deploy NFTStaking contract
         NFTStaking nftStaking = new NFTStaking(initialOwner);
